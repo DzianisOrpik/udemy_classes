0395ba68ad0eca28b42d21b1b76df52a49de0856
Dzianis Orpik <Dzianis15Or@gmail.com>
2019-05-21 21:44:38 -0500

    May21

---
+++ script.js
@@ -0,0 +1,16 @@
+// alert("Hello User");
+// userName = prompt("Please enter your name");
+// userNumber_1 = prompt(userName + " Please enter your favorite first number: ");
+// userNumber_2 = prompt(userName + " Please enter your favorite second number: ");
+// alert(userName + " it your first favorite number: " + userNumber_1 + " and your second favorite number: " + userNumber_2);
+// alert(userName + "Your summ = " + userNumber_1 + userNumber_2);
+
+// var firstName = prompt("What is your first name? ")
+// var lastName = prompt("What is your last name? ")
+// var age = prompt("What is your age? ")
+// console.log("It is your full name:" + firstName + " " + lastName);
+// console.log("Your age: " + age);
+
+var age = prompt("What is your age? ")
+var days = age * 365;
+alert(age + " yers is roughly " + days + " days " );
